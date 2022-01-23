468332ab602ac2abc69284d34cd3ac3f428f3615
JDP\Arindam.Ghosh <arindam.ghosh@jdpa.com>
2022-01-23 16:05:59 -0500

    initial comitt

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+    entry: './src/index.tsx',
+    devtool: 'eval-source-map',
+    resolve: {
+      extensions: ['.js', '.ts', '.tsx'],
+    },
+    module: {
+      rules: [
+        // {
+        //   test: /\.tsx?$/,
+        //   loader: 'ts-loader',
+        //   exclude: /node_modules/,
+        // },
+        {
+          test: /\.tsx?$/,
+          loader: 'babel-loader',
+          exclude: /node_modules/,
+        },
+      ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+            filename: 'index.html'
+        })
+    ]
+}
